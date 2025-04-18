85b44f481d44acfe3aaf9b60a9b66dfb3532730b
Mateusz Kulewicz <mateusz.kulewicz@canonical.com>
2025-04-18 13:15:22 +0200

    feat: migrate to v1 CI

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,5 +1,7 @@
-# Copyright 2022 Jon Seager.
+# Copyright 2025 Canonical.
 # See LICENSE file for licensing details.
+import os
+from pathlib import Path
 
 import pytest
 from pytest_operator.plugin import OpsTest
@@ -7,5 +9,8 @@ from pytest_operator.plugin import OpsTest
 
 @pytest.fixture(scope="module")
 async def charm_under_test(ops_test: OpsTest):
+    if charm_file := os.environ.get("CHARM_PATH"):
+        return Path(charm_file)
+
     charm = await ops_test.build_charm(".")
     return charm
