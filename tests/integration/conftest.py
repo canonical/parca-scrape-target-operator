393dad3074ca1d571f487717f1b0169351f864db
Mateusz Kulewicz <mateusz.kulewicz@canonical.com>
2025-05-01 16:46:16 +0200

    chore: Migrate integration tests to jubilant (#86)
    * chore: Migrate integration tests to jubilant
    
    * chore: log cleanup
    
    * fix: wrong comment
    
    * fix: error on blocked

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,16 +1,30 @@
 # Copyright 2025 Canonical.
 # See LICENSE file for licensing details.
+import logging
 import os
+import subprocess
 from pathlib import Path
 
 import pytest
-from pytest_operator.plugin import OpsTest
+from pytest_jubilant import pack_charm
+
+logger = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope="module")
-async def charm_under_test(ops_test: OpsTest):
+def scrape_target_charm() -> Path:
     if charm_file := os.environ.get("CHARM_PATH"):
         return Path(charm_file)
 
-    charm = await ops_test.build_charm(".")
-    return charm
+    # Intermittent issue where charmcraft fails to build the charm for an unknown reason.
+    # Retry building the charm
+    for _ in range(3):
+        logger.info("packing...")
+        try:
+            pth = pack_charm().charm.absolute()
+        except subprocess.CalledProcessError:
+            logger.warning("Failed to build the charm. Trying again!")
+            continue
+        os.environ["CHARM_PATH"] = str(pth)
+        return pth
+    raise err  # noqa
